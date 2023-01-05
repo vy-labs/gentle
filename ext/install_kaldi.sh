c4d5d81bdabe3eec5d25170b5d4dc1fecf546eba
Dharmendra Verma <dharmendra@synaptic.com>
2023-01-05 14:59:11 +0530

    Update install_kaldi.sh

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -2,11 +2,11 @@
 
 # Prepare Kaldi
 cd kaldi/tools
-make clean
 make
 ./extras/install_openblas.sh
 cd ../src
 # make clean (sometimes helpful after upgrading upstream?)
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
+make clean
 make depend
 cd ../../
